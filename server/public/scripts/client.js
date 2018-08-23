97d0ca2927c118ce4316546bc0e937f361301ebc
Lars <larsmackenzie@gmail.com>
2018-08-23 10:00:51 -0500

    created addMovie function and html input form

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,9 +2,12 @@ const app = angular.module('MovieApp', ['ngRoute']);
 
 app.config(function($routeProvider){
     $routeProvider.when('/', {
+        template: '<h2>Movie Collection Home</h2>'
+    })
+    .when('/movies', {
         templateUrl: 'views/movies.html',
         controller: 'MovieController as mc'
-    })
+    })  
     .when('/genres', {
         templateUrl: 'views/genres.html', 
         controller: 'GenreController as gc'
--- server/public/scripts/controllers/moviecontroller.js
+++ server/public/scripts/controllers/moviecontroller.js
@@ -1,5 +1,26 @@
 app.controller('MovieController', function($http){
     console.log('in MovieController');
     let vm = this; 
-    vm.message = 'hi';
+    
+    vm.addMovie = function(){
+        let movieToAdd = {
+            title: vm.movieTitle, 
+            release_date: vm.movieDate,
+            run_time: vm.movieTime,
+            image_url: vm.movieUrl, 
+            genre_id: vm.movieGenre
+        }
+        $http({
+            method: 'POST', 
+            url: '/movies', 
+            data: movieToAdd
+        }).then(function(){
+            //get movies 
+        }).catch(function(error){
+            console.log('Error posting movie', error);
+            alert('There was an error posting the movie.'); 
+        })
+    }//end addMovie
+
+
 })
