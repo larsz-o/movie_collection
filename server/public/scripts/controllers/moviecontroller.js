6f8fc41f023dc4d6bdf24e1c18499a7cf8ec8619
Lars <larsmackenzie@gmail.com>
2018-08-23 10:36:04 -0500

    movies GET route retreives movies from database, with getMovies()

--- server/public/scripts/controllers/moviecontroller.js
+++ server/public/scripts/controllers/moviecontroller.js
@@ -1,26 +1,42 @@
-app.controller('MovieController', function($http){
+app.controller('MovieController', function ($http) {
     console.log('in MovieController');
-    let vm = this; 
-    
-    vm.addMovie = function(){
+    let vm = this;
+    vm.movieCollection = [];
+
+    vm.addMovie = function () {
         let movieToAdd = {
-            title: vm.movieTitle, 
+            title: vm.movieTitle,
             release_date: vm.movieDate,
             run_time: vm.movieTime,
-            image_url: vm.movieUrl, 
+            image_url: vm.movieUrl,
             genre_id: vm.movieGenre
         }
         $http({
-            method: 'POST', 
-            url: '/movies', 
+            method: 'POST',
+            url: '/movies',
             data: movieToAdd
-        }).then(function(){
-            //get movies 
-        }).catch(function(error){
+        }).then(function () {
+            getMovies(); 
+            vm.movieTitle ='';
+            vm.movieGenre = '';
+            vm.movieDate = '';
+            vm.movieTime = '';
+            vm.movieUrl = '';
+        }).catch(function (error) {
             console.log('Error posting movie', error);
-            alert('There was an error posting the movie.'); 
+            alert('There was an error posting the movie.');
         })
     }//end addMovie
-
-
+    function getMovies() {
+        $http({
+            method: 'GET',
+            url: '/movies'
+        }).then(function (response) {
+            vm.movieCollection = response.data;
+        }).catch(function(error){
+            console.log('Error getting movies', error);
+            alert('There was an error retrieving the movies'); 
+        })
+    }// end getMovies 
+getMovies(); 
 })
--- server/routers/movies-router.js
+++ server/routers/movies-router.js
@@ -3,9 +3,20 @@ const router = express.Router();
 const pool = require('../modules/pool.js'); 
 
 //GET routes
+router.get('/', (req, res)=>{
+    console.log('in movies GET route');
+    const query = `SELECT "movies"."title", "movies"."release_date", "movies"."run_time", "movies"."image_url", "genres"."genre" 
+    FROM "movies" JOIN "genres" ON "movies"."genre_id" = "genres"."id";`;
+    pool.query(query).then((results)=>{
+        console.log(results);
+        res.send(results.rows); 
+    }).catch((error)=>{
+        console.log('Error making GET request', error);
+        res.sendStatus(500); 
+    })
+})// end GET movies route
 
 //POST routes
-
 router.post('/', (req, res)=>{
     console.log('in movies POST route');
     const newMovie = req.body; 
